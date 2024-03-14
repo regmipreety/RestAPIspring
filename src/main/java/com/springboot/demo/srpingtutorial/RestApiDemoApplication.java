e76ee907e8528c2614725bac5f26a54ffb0bc505
regmipreety <priety.regmi46@gmail.com>
2024-03-14 10:50:49 -0500

    first commit for Rest API

---
+++ src/main/java/com/springboot/demo/srpingtutorial/RestApiDemoApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.srpingtutorial;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class RestApiDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestApiDemoApplication.class, args);
+	}
+
+
+
+}
